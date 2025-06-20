e30fc08e949a19ae12ca646618aa93042c6edcd5
rksaykot <rksaikatkhan999@gmail.com>
2025-06-20 21:25:26 +0600

    50% done.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            clifford: '#da373d',
+            bg_priamry:'rgba(231, 111, 81, 0.1)',
+          }
+        }
+      }
+    }
